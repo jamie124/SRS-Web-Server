dda11b6ef887d23b632e5dc67d705e8ba9c21ec4
unknown <james-whitwell@.talos.local>
2011-03-28 21:54:03 +1300

    Updated srsdb setup script

--- srsdb.sql
+++ srsdb.sql
@@ -16,6 +16,63 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `questions`
+--
+
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_type` varchar(15) NOT NULL,
+  `question_title` varchar(120) NOT NULL,
+  `question_answer` varchar(120) DEFAULT NULL,
+  `possible_answer_one` varchar(120) DEFAULT NULL,
+  `possible_answer_two` varchar(120) DEFAULT NULL,
+  `possible_answer_three` varchar(120) DEFAULT NULL,
+  `possible_answer_four` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`question_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions`
+--
+
+LOCK TABLES `questions` WRITE;
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'sa','test question',NULL,NULL,NULL,NULL,NULL),(2,'sa','This is a demo.',NULL,NULL,NULL,NULL,NULL),(3,'tf','There are seven days in a week?',NULL,NULL,NULL,NULL,NULL),(4,'mc','Ubuntu is a what?','Operating System','Video Game','Operating System','Food',NULL);
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_question`
+--
+
+DROP TABLE IF EXISTS `user_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_question` (
+  `rec_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  PRIMARY KEY (`rec_id`),
+  KEY `question_id` (`question_id`),
+  CONSTRAINT `question_id` FOREIGN KEY (`question_id`) REFERENCES `questions` (`question_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_question`
+--
+
+LOCK TABLES `user_question` WRITE;
+/*!40000 ALTER TABLE `user_question` DISABLE KEYS */;
+INSERT INTO `user_question` VALUES (1,1,2),(2,4,4),(3,1,3),(4,1,1),(5,4,2),(6,4,1);
+/*!40000 ALTER TABLE `user_question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -44,7 +101,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'James','James','Whitwell','Programming','iOS','','Student','P@ssword1',NULL,NULL),(4,'test','test','user','userClass','Win7','null','Student','P@ssword1',NULL,NULL);
+INSERT INTO `users` VALUES (1,'James','James','Whitwell','Programming','iOS','','Student','P@ssword1','2011-03-28 20:26:59',1),(4,'test','test','user','userClass','Win7','null','Student','P@ssword1',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +114,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-03-26 12:53:06
+-- Dump completed on 2011-03-28 21:52:58
